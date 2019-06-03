dcc6efe7c7dc3d675b7c239a554bf518fd93074c
Antoine Proux <aprouxdev@gmail.com>
2019-06-03 11:55:18 +0200

    Initial Commit

---
+++ MyVinylCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyVinylCollection
+//
+//  Created by Antoine Proux on 03/06/2019.
+//  Copyright © 2019 Antoine Proux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
