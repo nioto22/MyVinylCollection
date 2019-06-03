a9d072ab85a85fbcede13afd8e91449c67c9876e
Antoine Proux <aprouxdev@gmail.com>
2019-06-03 18:43:35 +0200

    First commit : homeViewController storyboard started

---
+++ MyVinylCollection/Utils/RoundedUIImage.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedUIImage.swift
+//  MyVinylCollection
+//
+//  Created by Antoine Proux on 03/06/2019.
+//  Copyright © 2019 Antoine Proux. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedUIImage: UIImage {
+
+    @IBInspectable var borderColor: UIColor = UIColor.white {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 2.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+}
